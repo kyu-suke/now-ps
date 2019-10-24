4edbcdd5e059c24c277c91c22cbab4d5bd05baaf
kyu-suke <ykaneoka7@gmail.com>
2019-10-24 23:38:57 +0900

    methods

--- src/Respo.purs
+++ src/Respo.purs
@@ -1,15 +1,9 @@
 module Respo (respo,  hndl) where
 
-import Data.Function.Uncurried (Fn2, runFn2)
 import Node.HTTP
-
 
 respo :: String
 respo = "hello from purescript"
-
---foreign import hogeImpl :: Fn2 String String String
---fuga :: String -> String -> String
---fuga x y = runFn2 hogeImpl x y
 
 hndl :: Request -> String
 hndl req = requestURL req
